31f17ec95aa91b8f6944bf1c98cc1c72fa43bb14
sim0n-sys <85111243+sim0n-sys@users.noreply.github.com>
2021-08-27 06:26:02 +0300

    Update DB.java

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -4,4 +4,10 @@ public class DB {
 
    String connectionString = "jdbc:postgresql://localhost:5432/organization_api"; //!
    Sql2o sql2o = new Sql2o(connectionString, "simon", "simonalex"); //!
+   
+//    String connectionString = "jdbc:postgresql://\n" +
+//            "ec2-34-240-75-196.eu-west-1.compute.amazonaws.com/\n" +
+//            "d17jl56esc99c3"; //!
+//    Sql2o sql2o = new Sql2o(connectionString, "\n" +
+//            "fpbqzndntbkonx", "8ce73b18fc819ee6eb1dff35c953c13742cfc290d301665f8249f6fb9a8c998f"); //!
 }
